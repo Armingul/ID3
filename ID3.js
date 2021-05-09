0c12e3731a882d0c7a0d5319ddf784375a122f28
Armingul <svillarr@ucm.es>
2021-05-09 18:44:53 +0200

    commit final

---
+++ ID3.js
@@ -0,0 +1,79 @@
+var Node = require("tree-node");
+var _ = require('underscore');
+
+class ID3 {
+    
+    constructor(decision){
+        this.decision = decision;
+    }
+
+    //Implementacion del algoritmo ID3 con atributos de entrada
+    //Lista de ejemplos y lista de atributos
+    id3algorithm(atributos, trainingData, node, comingFrom){
+        let newNode = new Node();
+        newNode.data("comingFrom", comingFrom);
+        node.appendChild(newNode);
+    
+        if(trainingData.length <= 0){
+            newNode.data("name", "no data");
+            newNode.data("branches", []);
+    
+        } else if (trainingData.every(data => data[this.decision].toUpperCase() === "SI")){
+            newNode.data("name", "+");
+            newNode.data("branches", []);
+    
+        } else if (trainingData.every(data => data[this.decision].toUpperCase() === "NO")){
+            newNode.data("name", "-");
+            newNode.data("branches", []);
+    
+        } else {
+            let meritos = this.calculaMeritos(atributos, trainingData);
+            let min = {merito: Number.MAX_SAFE_INTEGER};
+            meritos.forEach(val => {
+                if (val.merito < min.merito){
+                    min = val;
+                }
+            });
+            newNode.data("name", min.name);
+            newNode.data("branches", min.branches);
+            min.branches.forEach(branch =>{
+                this.id3algorithm(atributos.filter(attr => attr.toUpperCase() !== min.name.toUpperCase()), trainingData.filter(data => data[min.name].toUpperCase() === branch.toUpperCase()), newNode, branch);
+            });
+        }
+    }
+    
+    //Algoritmo que calcula si los atributos son viables o no
+     calculaMeritos(atributos, trainingData){
+        let claves = atributos.slice(0, atributos.length - 1);
+        let meritos = [];
+        claves.forEach(clave => {
+            let branches = _.uniq(trainingData.map(element => element[clave]));
+            let merito = 0;
+            branches.forEach(branch => {
+                let a = trainingData.filter(data => data[clave] === branch).length;
+                let p = trainingData.filter(data => data[clave] === branch && data[this.decision].toUpperCase() === "SI").length / a;
+                let n = trainingData.filter(data => data[clave] === branch && data[this.decision].toUpperCase() === "NO").length / a;
+                let r = a / trainingData.length;
+                merito += (r *this.entropia(p, n));
+            })
+            meritos.push({name: clave, merito: merito, branches: branches});
+        });
+        return meritos;
+    }
+    
+    //Calculamos la entropia a partir de p y n
+     entropia(p, n){
+        return -p*this.fixedLog(p) - n*this.fixedLog(n);
+    }
+    //Algoritmo en base 2 con ajuste ya que al hacer el logaritmo de 
+    //0 el resultado era 0 y no se podia multiplicar por 0.
+     fixedLog (number){
+        return Math.log2(number) === -Infinity ? Number.MAX_SAFE_INTEGER : Math.log2(number)
+    }
+}
+
+
+
+module.exports = {
+    ID3: ID3
+}
---
+++ utils.js
@@ -0,0 +1,69 @@
+var path = require("path");
+var fs = require('fs');
+
+module.exports = {
+
+    //Funcion auxiliar para reemplazar los saltos de lineas por ',' y quitas las posiciones vacias
+    readAndParseToArray: function (fileName){
+        let content;
+        try{
+            content = fs.readFileSync(path.resolve(__dirname, fileName), 'UTF-8');
+        } catch (err) {
+            console.log("Error leyendo el archivo " + fileName);
+            return [];
+        }
+        return content.replace(/(\r\n)/g,",").split(",").filter(data => data !== "");
+    },
+    
+    //Funcion auxiliar para pintar el arbol
+    transformNodesToPrintable: function (tree, printable){
+        
+        if (tree.data.name === "+" || tree.data.name === "-" || tree.data.name === "no data") {
+            printable.push([tree.data.comingFrom, tree.data.name.toUpperCase()]);
+        } else if (tree.childIdsList.every(childkey => tree.childs[childkey].data.name === "+" || tree.childs[childkey].data.name === "-" || tree.childs[childkey].data.name === "no data")) {
+            let branch = []
+            branch.push(tree.data.comingFrom);
+            let leaf = [];
+            leaf.push(tree.data.name.toUpperCase());
+            tree.childIdsList.forEach(childkey => leaf.push([tree.childs[childkey].data.comingFrom, tree.childs[childkey].data.name.toUpperCase()]));
+            branch.push(leaf);
+            printable.push(branch);
+    
+        } else {
+            tree.childIdsList.forEach(childkey => {
+                this.transformNodesToPrintable(tree.childs[childkey],printable)
+            });
+        }
+    },
+
+    parseArrayToTrainingData: function (atributos, data){
+        let trainingData = [];
+        for (let i = 0; i < data.length - atributos.length + 1; i++){
+            let trainingItem = {};
+            for (let j = 0; j < atributos.length; j++){
+                trainingItem[atributos[j]] = data[i];
+                i++;
+            }
+            i--;
+            trainingData.push(trainingItem);
+        }
+        return trainingData;
+    },
+
+    guessResult: function (atributos, arbol){
+        atributos = atributos.map(atrib => atrib.toUpperCase());
+        for(branch in arbol.data.branches){
+            if(atributos.indexOf(arbol.data.branches[branch].toUpperCase()) !== -1){
+                let childid = arbol.childIdsList.filter(id => arbol.childs[id].data.comingFrom.toUpperCase() === arbol.data.branches[branch].toUpperCase());
+                if(arbol.childs[childid].data.name === "+" || arbol.childs[childid].data.name === "-" || arbol.childs[childid].data.name === "no data"){
+                    return arbol.childs[childid].data.name;
+                } else {
+                    atributos.splice(atributos.indexOf(arbol.data.branches[branch].toUpperCase()), 1);
+                    return this.guessResult(atributos, arbol.childs[childid])
+                }
+            }
+        }
+    }
+
+}
+
